f6c6bacdc24611fe3e2f8305186daf94baf3f053
nonklhhgg <147769818+nonklhhgg@users.noreply.github.com>
2023-11-29 20:47:39 +0100

    Create word_utils.py

---
+++ word_utils.py
@@ -0,0 +1,29 @@
+from nltk.corpus import wordnet
+from nltk import download
+from nltk.corpus import words
+from nltk.metrics import edit_distance
+from random import sample
+
+download('wordnet')
+download('words')
+
+class WordExplorer:
+    @staticmethod
+    def get_meaning(word):
+        # Implementation for getting meanings
+        pass
+
+    @staticmethod
+    def get_synonyms(word):
+        # Implementation for getting synonyms
+        pass
+
+    @staticmethod
+    def get_antonyms(word):
+        # Implementation for getting antonyms
+        pass
+
+    @staticmethod
+    def correct_spelling(word):
+        # Implementation for spelling correction
+        pass
