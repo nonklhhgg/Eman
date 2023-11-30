c4e03e7e41cf99cf111f48d6fcec9aac2ddd58cd
nonklhhgg <147769818+nonklhhgg@users.noreply.github.com>
2023-11-30 09:24:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
 from nltk.chat.util import Chat, reflections
-from word_utils import get_meaning, get_synonyms, get_antonyms
+from responses import pairs, get_meaning, get_synonyms, get_antonyms
 
-def main():
-    print("Welcome to Word Explorer Chatbot!")
-    chat_pairs = [
-        # Define your chat patterns and responses here
-        # Example: (r'meaning (.*)', get_meaning),
-        #          (r'synonyms (.*)', get_synonyms),
-        #          (r'antonyms (.*)', get_antonyms),
-    ]
-
-    chatbot = Chat(chat_pairs, reflections)
-    print("Type 'quit' to exit.")
+def chat():
+    # Print a welcome message
+    print("Hi! I am a chatbot. How can I help you today?")
+    
+    # Create a Chat instance with predefined pairs and reflections
+    chat_instance = Chat(pairs, reflections)
 
     while True:
-        user_input = input("You: ")
-        if user_input.lower() == 'quit':
+        try:
+            # Get user input
+            user_input = input("> ")
+            
+        except KeyboardInterrupt:
             print("Goodbye!")
             break
-
-        response = chatbot.respond(user_input)
-        print("Chatbot:", response)
+            
+        # If the user input is "quit", exit the loop
+        if user_input.lower() == "quit":
+            print("Goodbye!")
+            break
+        else:
+            # Remove leading/trailing whitespace
+            user_input = user_input.strip()
+            
+            # If the user input ends with a question mark
+            if user_input.endswith("?"):
+                # Get a response from the Chat instance
+                response = chat_instance.respond(user_input)
+            else:
+                # If the user input does not end with a question mark
+                # Split the input into a list of words
+                words = user_input.split()
+                
+                # If the word "meaning" is present in the input
+                if "meaning" in words:
+                    word = words[-1].rstrip("s")
+                    response = get_meaning(word)
+                elif "synonyms" in words:
+                    word = words[-1].rstrip("s")
+                    response = get_synonyms(word)
+                elif "antonyms" in words:
+                    word = words[-1].rstrip("s")
+                    response = get_antonyms(word)
+                else:
+                    # If none of the specific keywords are present in the input
+                    # Get a general response from the Chat instance
+                    response = chat_instance.respond(user_input)
+            
+            # Print the response
+            print(response)
 
 if __name__ == "__main__":
-    main()
+    # Call the chat function
+    chat()
